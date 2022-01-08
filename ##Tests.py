377249e36eaa964e709dfb8a286ed30505093eab
billy <joro2100@naver.com>
2022-01-08 09:02:10 +0900

    study_20220108

--- ##Tests.py
+++ ##Tests.py
@@ -3,4 +3,4 @@ import pandas as pd
 df=pd.read_csv('http://bit.ly/ds-korean-idol')
 new_columns=['name','group','소속사', '성별', '생년월일', '키', '혈액형', '브랜드평판지수']
 df.columns=new_columns
-print(df.sort_index())
+print(df.sort_values(by='생년월일'))
