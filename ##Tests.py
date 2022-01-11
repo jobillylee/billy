23c6c1b8788247c7e9bb1f9aef28edbb6ccf70d0
billy <joro2100@naver.com>
2022-01-11 10:12:22 +0900

    study_220111

--- ##Tests.py
+++ ##Tests.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv('https://bit.ly/ds-korean-idol')
-df2=df.copy()
-df2_drop=df2.drop([1,3],axis=0)
-print(df2_drop.head())
+df2 = pd.read_csv('https://bit.ly/ds-korean-idol-2')
+df2_drop=df2.drop([1,3,5],axis=0)
+df_merge=pd.merge(df, df2_drop, on='이름', how='outer')
+print(df_merge.head())
