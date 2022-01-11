56acc774d4329f41eff99107c79f2eb7b9212b11
billy <joro2100@naver.com>
2022-01-11 19:59:22 +0900

    study_220111

--- ##Tests.py
+++ ##Tests.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv('https://bit.ly/ds-korean-idol')
 df2 = pd.read_csv('https://bit.ly/ds-korean-idol-2')
-df2_drop=df2.drop([1,3,5],axis=0)
-df_merge=pd.merge(df, df2_drop, on='이름', how='outer')
-print(df_merge.head())
+my_map={'남자':1, '여자':0}
+df['성별']=df['성별'].map(my_map)
+print(df.head())
